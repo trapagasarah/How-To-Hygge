ec2b53ec37348055fed0299e8f490bef0a76f91a
Sarah Trapaga <trapagasarah@gmail.com>
2019-05-20 15:39:54 -0400

    styled account

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -16,9 +16,14 @@ const AboutWrapper = styled.div`
          h1 {
              font-size: 4em;
          }
+        
+         h2 {
+             font-size: 2.5em;
+         }
 
          p {
              font-size: 2em;
+             margin-bottom: 2em;
          }
          
          .about-hygge {
@@ -33,7 +38,10 @@ class About extends Component {
                 <div className="about-hygge">
                     <h1>What is Hygge?</h1>
                     <p>"Hygge is about an atmosphere and an experience, rather than about things. It is aabout being with the people we love, A feeling of home. Aa feeling that we are safe, that we are shielded from the world and allow ouorselves to let our guard down."</p>
+                    <h2>Credit Source Material</h2>
+                    <p>The information for this website references <a href="https://www.amazon.com/Little-Book-Hygge-Danish-Secrets/dp/0062658808/ref=sr_1_1?keywords=the+little+book+of+hygge&qid=1558380578&s=gateway&sr=8-1">The Little Book of Hygge</a> written by Meik Wiking</p>
                 </div>
+
                 <img src="/images/about-combined.JPG" />
             </AboutWrapper>
         )
--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -2,7 +2,28 @@ import React, {Component} from 'react'
 import styled from 'styled-components'
 
 const AccountWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 2em 0;
+    font-family: 'Anonymous Pro', monospace;
     color: white;
+    margin-top: 3em;
+
+    button {
+        background-color: rgb(187, 200, 147);
+        border: 1px solid white;
+        width: 9em;
+        height: 1.5em;
+        margin-bottom: 1em;
+        padding: 0;
+    }
+
+    button:hover {
+        color: rgb(187, 200, 147);
+        background-color: white;
+    }
 `
 
 class Account extends Component {
@@ -11,7 +32,7 @@ class Account extends Component {
             <AccountWrapper>
                 <h3>Name: {this.props.user.name}</h3>
                 <h3>Email: {this.props.user.email}</h3>
-                <button onClick={this.props.deleteUser}>Delete Account</button>
+                <button className="btn btn-primary" onClick={this.props.deleteUser}>Delete Account</button>
             </AccountWrapper>
         )
     }
